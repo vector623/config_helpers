0f48e7427fc60036b4b1e4647cda765905ce6199
David Gallmeier <dgallmeier@gmail.com>
2018-04-18 13:42:43 -0400

    Add input and output comments

--- generate_environment_variables_batchfilev3.ps1
+++ generate_environment_variables_batchfilev3.ps1
@@ -2,6 +2,15 @@
 
 param([string] $environments_variables_file)
 
+<# contents of environment_variables.sh
+export DataSource=databaseserver.com
+export InitialCatalog=DataBase
+export IntegratedSecurity=False
+export ConnectionTimeout=1000
+export User=db_user
+export Password=db_password
+#>
+
 $environment_variables_batchlines = Get-Content -Path $environments_variables_file |
     Where-Object {$_ -like "export*"} |
     Select-Object @{ 
@@ -18,3 +27,12 @@ $environment_variables_batchlines = Get-Content -Path $environments_variables_fi
 $environment_variables_batchlines | 
     Select -Expand batchline | 
     Out-File -FilePath 'environment_variables.bat'
+
+<# resulting environment_variables.bat file
+setx -m DataSource databaseserver.com
+setx -m InitialCatalog DataBase
+setx -m IntegratedSecurity False
+setx -m ConnectionTimeout 1000
+setx -m User db_user
+setx -m Password db_password
+#>
